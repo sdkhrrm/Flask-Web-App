ada42ea9c4eed7941a7944feeeccae074b726196
Saadia Khurram <saadia.khurram@outlook.com>
2025-08-24 23:31:00 +0200

    Added class Note

--- website/models.py
+++ website/models.py
@@ -1,5 +1,12 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
